bb226975daebb5296144116c6132caa0d03fcfb1
Sariiituus <sara.rodriguez@bootcamp-upgrade.com>
2022-05-23 21:08:56 +0200

    interación#2

--- 2.Destructuring.js
+++ 2.Destructuring.js
@@ -4,11 +4,27 @@ no hace falta hacer destructuring del array, solo del objeto. */
 
 const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
 
+let {title, gender, year} = game;
+
+console.log('Título:', title);
+console.log('Género:', gender);
+console.log('Año:', year);
+
+
+
 /* 2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
 llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
 imprimelo por consola. */
 
 const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+let [fruit1, fruit2, fruit3] = fruits;
+
+console.log('Fruta 1:', fruit1);
+console.log('Fruta 2:', fruit2);
+console.log('Fruta 3:', fruit3);
+
+
 /* 
 2.3 En base al siguiente javascript, usa destructuring para crear 2 
 variables igualandolo a la función e imprimiendolo por consola. */
@@ -17,9 +33,28 @@ const animalFunction = () => {
     return {name: 'Bengal Tiger', race: 'Tiger'}
 };
 
+let {name} = animalFunction();
+let {race} = animalFunction();
+
+console.log('Nombre:', name);
+console.log('Raza:', race);
+
+
 /* 2.4 En base al siguiente javascript, usa destructuring para crear las 
 variables name y itv con sus respectivos valores. Posteriormente crea 
 3 variables usando igualmente el destructuring para cada uno de los años 
 y comprueba que todo esta bien imprimiendolo. */
 
-const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+const car = {carName: 'Mazda 6', itv: [2015, 2011, 2020] }
+
+let {carName,itv} = car;
+
+let [year1, year2, year3] = itv;
+
+
+console.log('Marca:',carName);
+console.log('ITV:', itv);
+
+console.log('ITV 1:', year1);
+console.log('ITV 2:', year2);
+console.log('ITV 3:', year3);
--- 3.Spread Operator.js
+++ 3.Spread Operator.js
@@ -1,19 +1,33 @@
 //3.1 Dado el siguiente array, crea una copia usando spread operators.
 const pointsList = [32, 54, 21, 64, 75, 43]
+let copyPointsList = [...pointsList];
+
+console.log(copyPointsList);
+
 
 //3.2 Dado el siguiente objeto, crea una copia usando spread operators.
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+let copyToy = {...name,...date,...color};
+
+console.log(copyToy);
+
 
 /* 3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
 spread operatos. */
-const pointsList = [32, 54, 21, 64, 75, 43];
-const pointsLis2 = [54,87,99,65,32];
+const pointsList2 = [32, 54, 21, 64, 75, 43];
+const pointsLis3 = [54,87,99,65,32];
+
+
+
 
 /* 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
 con spread operators. */
-const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toy2 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
 
+
+
+
 /* 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
-pero sin editar el array inicial. De nuevo, usando spread operatos. */
+pero sin editar el array inicial. De nuevo, usando spread operators. */
 const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
