4fd67cc2618ff371bbfd18d7a3123a2d64498b44
Sariiituus <sara.rodriguez@bootcamp-upgrade.com>
2022-05-23 21:22:58 +0200

    iteracion#3

--- 3.Spread Operator.js
+++ 3.Spread Operator.js
@@ -7,7 +7,7 @@ console.log(copyPointsList);
 
 //3.2 Dado el siguiente objeto, crea una copia usando spread operators.
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
-let copyToy = {...name,...date,...color};
+let copyToy = {...toy};
 
 console.log(copyToy);
 
@@ -15,9 +15,11 @@ console.log(copyToy);
 /* 3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
 spread operatos. */
 const pointsList2 = [32, 54, 21, 64, 75, 43];
-const pointsLis3 = [54,87,99,65,32];
+const pointsList3 = [54,87,99,65,32];
 
+let newPointsList = [...pointsList2, ...pointsList3];
 
+console.log(newPointsList);
 
 
 /* 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
@@ -25,9 +27,16 @@ con spread operators. */
 const toy2 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
 
+let newObject = {...toy2, ...toyUpdate};
 
+console.log(newObject);
 
 
 /* 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
 pero sin editar el array inicial. De nuevo, usando spread operators. */
 const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+let newColors = [...colors];
+newColors.splice(2,1);
+
+console.log(newColors);
