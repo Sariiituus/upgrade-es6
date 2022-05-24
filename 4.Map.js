da1773a2b6bd41c4325cc24e7b8148ef48c22ff9
Sariiituus <sara.rodriguez@bootcamp-upgrade.com>
2022-05-24 13:18:12 +0200

    iteración#4

--- 4.Map.js
+++ 4.Map.js
@@ -7,6 +7,12 @@ const users = [
 	{id:4, name: 'Amanda'}
 ];
 
+
+let usersName = users.map(user => user.name); 
+
+console.log(usersName);
+
+
 // 4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
 // de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
 // empiece por 'A'.
@@ -17,6 +23,20 @@ const users2 = [
 	{id:4, name: 'Amanda'}
 ];
 
+let usersName2 = users2.map(user => {
+	if (user.name.charAt(0) === 'A')
+	return 'Anacleto'; 
+
+	else 
+	return user.name;
+
+});
+
+
+console.log(usersName2);
+
+
+
 // 4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
 // de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
 // cuando el valor de la propiedad isVisited = true.
@@ -26,3 +46,14 @@ const cities = [
 	{isVisited:true, name: 'Amsterdam'}, 
 	{isVisited:false, name: 'Seul'}
 ];
+
+
+let nameCities = cities.map(city => {
+	if(city.isVisited === true)
+	return city.name + ': Visitado';
+
+	else
+	return city.name;
+});
+
+console.log(nameCities);
