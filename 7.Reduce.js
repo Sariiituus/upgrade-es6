fb0a31d84a2336de9a31e574abbf4e9e5a07553e
Sariiituus <sara.rodriguez@bootcamp-upgrade.com>
2022-05-24 17:05:51 +0200

    iteracion#7

--- 7.Reduce.js
+++ 7.Reduce.js
@@ -14,7 +14,29 @@ const exams = [
     {name: 'Keanu Reeves', score: 10}
 ];
 
+let total = exams.reduce( (acumulador, actual) => {
+    return acumulador + actual.score
+}, 0);
+
+console.log(total);
+
 // 7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
 // alumnos que esten aprobados usando la función .reduce().
+let total_de_solo_aprobados = exams.reduce( (acumulador, actual) => {
+    if (actual.score >= 5) {
+        return acumulador + actual.score
+    }
+    else {
+        return acumulador;
+    }
+}, 0);
+
+console.log(total_de_solo_aprobados);
 
 // 7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce().
+const tot =  exams.reduce( (acumulador, actual) => {
+    return acumulador + actual.score
+}, 0);
+
+const media = tot / exams.length;
+console.log(media);
