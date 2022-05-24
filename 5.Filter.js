bf7f6fa44e260381dd91d961bccc4112dd71d8a5
Sariiituus <sara.rodriguez@bootcamp-upgrade.com>
2022-05-24 17:04:39 +0200

    iteración#5

---
+++ 5.Filter.js
@@ -0,0 +1,136 @@
+// 5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean mayor que 18
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+
+let filterAge = ages.filter(item => item > 18);
+
+console.log(filterAge);
+
+
+// 5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean par.
+const ages2 = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+let par = [];
+
+for (const age of ages2) {
+	if (age % 2 === 0) {
+		par.push(age);
+	}
+}
+
+console.log(par);
+
+
+// 5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+]
+
+let newArray = streamers.filter((streamers) => {
+	if (streamers.gameMorePlayed.includes('League of Legends')){
+	return true;
+	}
+
+	else {
+	return false;
+
+	}
+});
+
+console.log(newArray);
+
+
+// 5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos 
+// usar la funcion .includes() para la comprobación.
+const streamers2 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+let newArray2 = streamers2.filter((streamers2) => {
+	if(streamers2.name.includes('a')) {
+		return true;
+	}
+
+	else {
+		return false;
+	}
+});
+
+console.log(newArray2);
+
+
+
+// 5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
+// el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
+// .includes() para la comprobación.
+// Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
+// .age sea mayor que 35.
+const streamers5 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const result5 = streamers
+	.filter(elemento => elemento.gameMorePlayed.includes("Legends"))
+	.map(elem => {
+		if (elem.age > 35) {
+			elem.gameMorePlayed = elem.gameMorePlayed.toUpperCase();
+		}
+
+		return elem;
+	});
+
+console.log("result5");
+console.log(result5);
+
+
+// 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+// los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+// introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+// introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+const streamers3 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+let inputStreamers = document.querySelector("[data-function=toFilterStreamers]");
+let btn = document.querySelector("[data-function=toShowFilterStreamers]");
+btn.addEventListener("click", () => {
+	let texto = inputStreamers.value;
+	let results6 = streamers3.filter(
+		item => item.name.includes(texto)
+	);
+
+	console.log("resultado6");
+	console.log(results6);
+});
+
+
+
+// 5.7 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+// los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+// introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si introduzco 'i', 
+// me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+// En este caso, muestra solo los streamers filtrados cuando hagamos click en el button.
+const streamers4 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+
